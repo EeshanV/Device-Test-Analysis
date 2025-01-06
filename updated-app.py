9ce461642052eabbe90c5e861b74eb507cbf269e
Eeshan <eeshan@fedora>
2025-01-06 06:35:12 +0000

    Removed updated-app.py

--- updated-app.py
+++
@@ -1,6 +0,0 @@
-page = st.sidebar.radio("Navigation", ["Main Dashboard", "Device Analysis", "Test Analysis"])
-
-if page == "Device Analysis":
-    switch_page("device_analysis")
-elif page == "Test Analysis":
-    switch_page("test_analysis") 
